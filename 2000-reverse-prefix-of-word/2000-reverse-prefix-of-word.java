b44fac23255080b4347bff4ae3d1015fa3a1bcc9
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-03-23 17:53:14 +0530

    Time: 0 ms (100%), Space: 43.3 MB (21.02%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int n=word.length();
+        int index=word.indexOf(ch);
+        if(index==-1){
+            return word;
+        }
+        char[]arr=word.toCharArray();
+        int left=0;
+        int right=index;
+        while(left<right){
+            char temp=arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+return new String(arr);
+     }
+}
