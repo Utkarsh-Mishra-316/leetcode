de74c795b87db638534716fe8baaa6920971b174
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-04-09 14:05:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,21 +10,63 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        ListNode temp=head;
-        ArrayList<Integer> arr=new ArrayList<>();
-        while(temp!=null){
-            arr.add(temp.val);
-            temp=temp.next;
+ if(head==null || head.next==null) return head;
+ ListNode mid=  findmid(head);
+ ListNode righthead=mid.next;
+ mid.next=null;
+
+  ListNode left=sortList(head);
+  ListNode right=sortList(righthead);
+  return merge(left,right);
+
+
+     }
+     
+     ListNode findmid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null &&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        Collections.sort(arr);
-        temp=head;
-        int i=0;
-        while(temp!=null){
-            temp.val=arr.get(i);
-            i++;
-            temp=temp.next;
+        return slow;
+     }
+     ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummynode=new ListNode(0);
+        ListNode curr=dummynode;
+        while(l1!=null && l2!=null){
+            if(l1.val>l2.val){
+                curr.next=l2;
+                l2=l2.next;
+            }
+             else{
+                curr.next=l1;
+                l1=l1.next;
+            }
+            curr=curr.next;
         }
-    return head;
-    }
-}
+        curr.next = (l1 != null) ? l1 : l2;
+        return dummynode.next;
+     }
+     }
+ //   brute force
+// class Solution {
+//     public ListNode sortList(ListNode head) {
+//         ListNode temp=head;
+//         ArrayList<Integer> arr=new ArrayList<>();
+//         while(temp!=null){
+//             arr.add(temp.val);
+//             temp=temp.next;
+//         }
+//         Collections.sort(arr);
+//         temp=head;
+//         int i=0;
+//         while(temp!=null){
+//             temp.val=arr.get(i);
+//             i++;
+//             temp=temp.next;
+//         }
+//     return head;
+//     }
+// }
 
