57d100736a0c48462b9d0c4e20f402c7e140917b
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-04-26 22:34:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -8,45 +8,63 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         if (head == null || head.next == null) return head;
+//         ListNode curr=head;
+       
+//         int count=1;
+//         List <Integer> oddp=new ArrayList<>();
+//         List<Integer> evenp=new ArrayList<>();
+//         while(curr!=null) {
+//           if(count%2!=0){
+//             oddp.add(curr.val);
+             
+//             count++;
+//           }
+//           else{
+//             evenp.add(curr.val);
+             
+//             count++;
+//           }
+//           curr=curr.next;
+//         }
+//        curr = head;
+        
+//         // Pehle saare ODD values bharo
+//         for (int val : oddp) {
+//             curr.val = val;
+//             if (curr.next != null) {
+//                 curr = curr.next;
+//             }
+//         }
+        
+//         // Phir saare EVEN values bharo
+//         for (int val : evenp) {
+//             curr.val = val;
+//             if (curr.next != null) {
+//                 curr = curr.next;
+//             }
+//         }
+
+//         return head;
+//     }
+// }
+
+
+
 class Solution {
     public ListNode oddEvenList(ListNode head) {
         if (head == null || head.next == null) return head;
-        ListNode curr=head;
-       
-        int count=1;
-        List <Integer> oddp=new ArrayList<>();
-        List<Integer> evenp=new ArrayList<>();
-        while(curr!=null) {
-          if(count%2!=0){
-            oddp.add(curr.val);
-             
-            count++;
-          }
-          else{
-            evenp.add(curr.val);
-             
-            count++;
-          }
-          curr=curr.next;
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenhead=even;
+        while(even!=null && even.next!=null){
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
         }
-       curr = head;
-        
-        // Pehle saare ODD values bharo
-        for (int val : oddp) {
-            curr.val = val;
-            if (curr.next != null) {
-                curr = curr.next;
-            }
-        }
-        
-        // Phir saare EVEN values bharo
-        for (int val : evenp) {
-            curr.val = val;
-            if (curr.next != null) {
-                curr = curr.next;
-            }
-        }
-
+        odd.next=evenhead;
         return head;
-    }
-}
+    }}
