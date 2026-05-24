d02c23c5dcc1921b34a193dc8c4869ef7f29ee63
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-05-24 11:01:57 +0530

    Time: 0 ms (100%), Space: 43.8 MB (15.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,27 +15,41 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-       Queue<TreeNode> queue=new LinkedList<>();
-       queue.add(root.left);
-       queue.add(root.right);
-       while(!queue.isEmpty()){
-        TreeNode left=queue.poll();
+        if(root==null) return true;
+        return ismirror(root.left,root.right);
+        }
+        public boolean ismirror(TreeNode r1 ,TreeNode r2){
+            if(r1==null && r2==null ){return true;}
+            if(r1==null || r2==null) return false;
+         boolean current=(r1.val==r2.val);
+         boolean leftside=ismirror(r1.left,r2.right);
+         boolean rightside=ismirror(r1.right,r2.left);
+         return current && leftside && rightside ;
         
-        TreeNode right=queue.poll();
-        if(left==null && right==null){
-            continue;
         }
-        if(left==null || right == null){
-            return false;
         }
-        if(left.val!=right.val){
-            return false;
-        }
-        queue.add(left.left);
-        queue.add(right.right);
-        queue.add(left.right);
-        queue.add(right.left);
-       }        
- return true;
-    }
-}
+//        Queue<TreeNode> queue=new LinkedList<>();
+//        queue.add(root.left);
+//        queue.add(root.right);
+//        while(!queue.isEmpty()){
+//         TreeNode left=queue.poll();
+        
+//         TreeNode right=queue.poll();
+//         if(left==null && right==null){
+//             continue;
+//         }
+//         if(left==null || right == null){
+//             return false;
+//         }
+//         if(left.val!=right.val){
+//             return false;
+//         }
+//         queue.add(left.left);
+//         queue.add(right.right);
+//         queue.add(left.right);
+//         queue.add(right.left);
+//        }        
+//  return true;
+
+//     }
+// }
