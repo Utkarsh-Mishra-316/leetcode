6ddffd1e2625bca5500c48c09219195b8a10d743
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-06-02 14:07:11 +0530

    Time: 10 ms (50.66%), Space: 59.1 MB (82.35%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,63 +10,40 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
- if(head==null || head.next==null) return head;
- ListNode mid=  findmid(head);
- ListNode righthead=mid.next;
- mid.next=null;
+      if(head==null || head.next==null) return head;
+      ListNode mid=getmid(head);
+      ListNode nextmid=mid.next;
+      mid.next=null;
+      ListNode left=sortList(head);
+      ListNode right=sortList(nextmid);
+      return merge(left,right);
 
-  ListNode left=sortList(head);
-  ListNode right=sortList(righthead);
-  return merge(left,right);
-
-
-     }
-     
-     ListNode findmid(ListNode head){
+    }
+    ListNode getmid(ListNode head){
         ListNode slow=head;
-        ListNode fast=head.next;
-        while(fast!=null &&fast.next!=null){
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
         return slow;
-     }
-     ListNode merge(ListNode l1,ListNode l2){
-        ListNode dummynode=new ListNode(0);
-        ListNode curr=dummynode;
+    }
+    ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(0);
+        ListNode curr=dummy;
         while(l1!=null && l2!=null){
-            if(l1.val>l2.val){
-                curr.next=l2;
-                l2=l2.next;
-            }
-             else{
+            if(l1.val<l2.val){
                 curr.next=l1;
                 l1=l1.next;
             }
+            else{
+                curr.next=l2;
+                l2=l2.next;
+            }
             curr=curr.next;
         }
-        curr.next = (l1 != null) ? l1 : l2;
-        return dummynode.next;
-     }
-     }
- //   brute force
-// class Solution {
-//     public ListNode sortList(ListNode head) {
-//         ListNode temp=head;
-//         ArrayList<Integer> arr=new ArrayList<>();
-//         while(temp!=null){
-//             arr.add(temp.val);
-//             temp=temp.next;
-//         }
-//         Collections.sort(arr);
-//         temp=head;
-//         int i=0;
-//         while(temp!=null){
-//             temp.val=arr.get(i);
-//             i++;
-//             temp=temp.next;
-//         }
-//     return head;
-//     }
-// }
-
+        if (l1 != null) curr.next = l1;
+        if (l2 != null) curr.next = l2;
+        return dummy.next;
+    }
+}
