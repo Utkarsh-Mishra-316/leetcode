25e1a12242e01552ad243db02ae69026843163ee
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-06-17 17:53:26 +0530

    Time: 4 ms (76.3%), Space: 55.5 MB (37.04%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='*' && str.length()>0){
+                str.deleteCharAt(str.length()-1);
+            }
+            else if(ch=='#'){
+                str.append(str.toString());
+            }
+            else if(ch=='%'){
+                str.reverse();
+            }
+            else if(Character.isLetter(ch)){
+                str.append(ch);
+            }
+           
+        } 
+         return str.toString();
+    }
+}
