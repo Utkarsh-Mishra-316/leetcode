f268d0e2d4c417d4fb202c09a18d44ad9ff5eb12
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-07-13 23:05:47 +0530

    Time: 1 ms (99.95%), Space: 69.2 MB (31.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,25 +1,44 @@
-class Solution {
-    public int minSubArrayLen(int target, int[] nums) {
-     int n=nums.length;
-     int count=Integer.MAX_VALUE;
-     int left=0;
-     int sum=0;
-     for(int right=0;right<n;right++){
-      sum+=nums[right];
-       while(sum>=target){
-        count=Math.min(count,right-left+1);
-        sum-=nums[left];
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+//      int n=nums.length;
+//      int count=Integer.MAX_VALUE;
+//      int left=0;
+//      int sum=0;
+//      for(int right=0;right<n;right++){
+//       sum+=nums[right];
+//        while(sum>=target){
+//         count=Math.min(count,right-left+1);
+//         sum-=nums[left];
         
-        left++;
+//         left++;
 
-       }
+//        }
        
     
-    }
-    if(count==Integer.MAX_VALUE){
-        return 0;
-       }  
+//     }
+//     if(count==Integer.MAX_VALUE){
+//         return 0;
+//        }  
         
-            return count;
+//             return count;
 
-}}
+// }}
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+ int n=nums.length;
+int left=0;
+int sum=0;
+int count=Integer.MAX_VALUE;
+for(int right=0;right<n;right++){
+sum+=nums[right];
+while(sum>=target){
+    count=Math.min(count,right-left+1);
+    sum-=nums[left];
+    left++;
+}
+}
+if(count==Integer.MAX_VALUE){
+    return 0;
+}
+return count;}
+    }
