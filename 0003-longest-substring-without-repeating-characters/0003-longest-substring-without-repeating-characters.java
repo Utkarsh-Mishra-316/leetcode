b050d5303d7382643f56c1a8166ccdfdded7a658
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-07-14 21:46:52 +0530

    Time: 7 ms (37.5%), Space: 46.6 MB (19.98%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,18 +1,35 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if(s.length()==0 || s==null){return 0;}
+//         int maxlength=Integer.MIN_VALUE;
+//         int left=0;
+//         HashSet<Character> set=new HashSet<>();
+//         for(int i=0;i<s.length();i++){
+//             while(set.contains(s.charAt(i))){
+// set.remove(s.charAt(left));
+// left++;
+//             }set.add(s.charAt(i));
+
+//            maxlength=Math.max(maxlength,i-left+1); 
+//         }
+// return maxlength;
+
+//     }
+// }
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0 || s==null){return 0;}
-        int maxlength=Integer.MIN_VALUE;
+    if(s.length()==0 || s==null) return 0;
+    int maxl=Integer.MIN_VALUE;
         int left=0;
         HashSet<Character> set=new HashSet<>();
         for(int i=0;i<s.length();i++){
             while(set.contains(s.charAt(i))){
-set.remove(s.charAt(left));
-left++;
-            }set.add(s.charAt(i));
-
-           maxlength=Math.max(maxlength,i-left+1); 
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(i));
+            maxl=Math.max(maxl,i-left+1);
         }
-return maxlength;
-
-    }
-}
+        return maxl;
+    }}
