236572114eb2b87f6307dac3e682173d2474b3ed
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-07-18 14:57:05 +0530

    Time: 17 ms (78.37%), Space: 45.6 MB (48.95%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,32 +1,72 @@
+// class Solution {
+//     public int threeSumClosest(int[] nums, int target) {
+//         Arrays.sort(nums);
+//          int current=nums[0]+nums[1]+nums[2];
+//         int sum=0;
+
+//       for(int i=0;i<nums.length-2;i++){
+
+//          int start=i+1;
+//         int end=nums.length-1;
+//         while(start<end){
+//        sum=nums[i]+nums[start]+nums[end];
+//        if(current==target){
+//         return current;
+//        }
+//        if( Math.abs(target-sum)<Math.abs(target-current)){
+//         current=sum;
+//        }  
+//         if(sum<target){
+//         start++;
+//        }
+//        else{
+//          end--;
+//          while(start<end && nums[end]==nums[end+1]) end--;
+        
+//        }}
+//        }
+//         return current;
+    
+//       }
+     
+// }
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        Arrays.sort(nums);
-         int current=nums[0]+nums[1]+nums[2];
-        int sum=0;
-
-      for(int i=0;i<nums.length-2;i++){
-
-         int start=i+1;
-        int end=nums.length-1;
-        while(start<end){
-       sum=nums[i]+nums[start]+nums[end];
-       if(current==target){
-        return current;
-       }
-       if( Math.abs(target-sum)<Math.abs(target-current)){
-        current=sum;
-       }  
-        if(sum<target){
-        start++;
-       }
-       else{
-         end--;
-         while(start<end && nums[end]==nums[end+1]) end--;
-        
-       }}
-       }
-        return current;
-    
-      }
-     
+Arrays.sort(nums);
+int current=nums[0]+nums[1]+nums[2];
+int sum=0;
+for(int i=0;i<nums.length-2;i++){
+int left=i+1;
+int right=nums.length-1;
+while(left<right){
+   sum=nums[i]+nums[left]+nums[right];
+   if(sum==target){
+return sum;
+   }
+   if(Math.abs(sum-target)<Math.abs(current-target)){
+    current=sum;
+   }
+   if(sum<target){
+    left++;
+   }
+   else{
+    right--;
+   }
+  
 }
+}
+return current;
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
