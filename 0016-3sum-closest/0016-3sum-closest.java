2a1f6ebea1938c2df72a2d6b950185cfc8e8ddff
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-08-19 22:57:31 +0530

    Time: 17 ms (78.07%), Space: 45.6 MB (49.89%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,41 +1,27 @@
- 
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-Arrays.sort(nums);
-int current=nums[0]+nums[1]+nums[2];
-int sum=0;
-for(int i=0;i<nums.length-2;i++){
-int left=i+1;
-int right=nums.length-1;
-while(left<right){
-   sum=nums[i]+nums[left]+nums[right];
-   if(sum==target){
-return sum;
-   }
-   if(Math.abs(sum-target)<Math.abs(current-target)){
-    current=sum;
-   }
-   if(sum<target){
+        if(nums.length<3) return -1;
+        Arrays.sort(nums);
+        int curr=nums[0]+nums[1]+nums[2];
+    for(int i=0;i<nums.length;i++){
+        int left=i+1;
+        int right=nums.length-1;
+        while(left<right){
+ int sum=nums[i]+nums[left]+nums[right];
+ if(sum==target){
+    return sum;
+ }
+ if(Math.abs(sum-target)<Math.abs(curr-target)){
+    curr=sum;
+ }
+ else if(sum<target){
     left++;
-   }
-   else{
+ }
+ else{
     right--;
-   }
-  
+ }
+         }
+    } 
+    return curr;  
+    }
 }
-}
-return current;
-    }}
-
-
-
-
-
-
-
-
-
-
-
-
-
