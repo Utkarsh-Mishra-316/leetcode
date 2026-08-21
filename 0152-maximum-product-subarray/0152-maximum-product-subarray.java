5aa22c5b9e6fb230e8b5107c340003fb9a84cf71
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-08-21 21:14:52 +0530

    Time: 2 ms (72.08%), Space: 47.2 MB (73.73%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,33 +1,20 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        // int pre=1;
-        // int suff=1;
-        // int ans=Integer.MIN_VALUE;
-        // int n=nums.length;
-        // for(int i=0;i<n;i++){
-        //     if(pre==0) pre=1;
-        //     if(suff==0) suff=1;
-        //     pre=pre*nums[i];
-        //     suff=suff*nums[n-i-1];
-        //     ans=Math.max(ans,Math.max(suff,pre));
-        // }
-        // return ans;
-    if(nums==null || nums.length==0) return 0; 
-    
-    int maxp=nums[0];
-    int minp=nums[0];
-    int result=nums[0];
-    for(int i=1;i<nums.length;i++){
-        int curr=nums[i];
-        if(curr<0){
-            int temp=maxp;
-            maxp=minp;
-            minp=temp;
+       
+        int max= nums[0];
+        int currmax=nums[0];
+int currmin=nums[0];
+        for(int i=1;i<nums.length;i++){
+            int curr=nums[i];
+            if(curr<0){
+                int temp=currmax;
+                currmax=currmin;
+                currmin=temp;
+            }
+            currmax=Math.max(curr,curr*currmax);
+            currmin=Math.min(curr,curr*currmin);
+            max=Math.max(max,currmax);
         }
-        maxp=Math.max(curr,maxp*curr);
-        minp=Math.min(curr,minp*curr);
-        result=Math.max(result,maxp);
-    }
-    return result;
+        return max;
     }
 }
