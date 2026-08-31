cd02b1e54b5475890cce7d5193a151ba8e7fccd4
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-08-31 21:51:59 +0530

    Time: 2 ms (100%), Space: 46.4 MB (85.56%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -1,29 +1,28 @@
 class Solution {
-     
-    public static void reverse(char []ch,int ind){
+    static void reverse(int rights,char [] s){
         int left=0;
-        int right= ind;
+        int right=rights;
         while(left<right){
-        char temp=ch[left];
-        ch[left]=ch[right];
-        ch[right]=temp;
-        left++;
-        right--;}
+            char ch=s[left];
+            s[left]=s[right];
+            s[right]=ch;
+            left++;
+            right--;
+        }
+       
     }
     public String finalString(String s) {
         char [] ch=s.toCharArray();
- 
- int ind=0;
-         for(int i=0;i<s.length();i++){
-           char c=s.charAt(i);
-            if(c =='i'){
-                reverse(ch,ind-1);
+        int ind=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='i'){
+                reverse(ind-1,ch);
             }
             else{
                 ch[ind++]=c;
             }
         }
-       return new String(ch,0,ind);
-}
- 
+        return new String(ch,0,ind);
     }
+}
