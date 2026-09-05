52fdb964b79b015d38856c905c37a61b3e2897b0
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-09-05 22:12:32 +0530

    Time: 18 ms (81.27%), Space: 46.2 MB (86.57%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -10,32 +10,23 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
-        if (head == null || head.next == null) return head;
-
-        // 1. Dummy node sorted list ke liye
-        ListNode dummy = new ListNode(0);
-        ListNode curr = head; // Yeh hamara 'Red' element hai
-
-        while (curr != null) {
-            // Har baar sorted part ko shuru se check karne ke liye pointer
-            ListNode prev = dummy;
-            
-            // Agla unsorted node save karein
-            ListNode nextNode = curr.next;
-
-            // 2. Sahi jagah dhundna (Black list mein)
-            while (prev.next != null && prev.next.val < curr.val) {
-                prev = prev.next;
-            }
-
-            // 3. In-place Insertion
-            curr.next = prev.next;
-            prev.next = curr;
-
-            // 4. Agle unsorted node par jayein
-            curr = nextNode;
+        if(head==null || head.next==null){
+            return head;
         }
+        ListNode dummy=new ListNode(0);
 
-        return dummy.next;
+        ListNode curr=head;
+         while(curr!=null){
+            ListNode pre=dummy;
+            ListNode nextnode=curr.next;
+            while( pre.next!=null && pre.next.val<curr.val){
+                pre=pre.next;
+            }
+             curr.next=pre.next;
+             pre.next=curr;
+             curr=nextnode;
+        }
+        
+return dummy.next;
     }
 }
