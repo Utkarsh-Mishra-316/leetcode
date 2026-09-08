010249264669408107954f8187c772f71973dfcc
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-09-08 22:05:41 +0530

    Time: 1 ms (100%), Space: 46.4 MB (83.02%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -9,39 +9,39 @@
  * }
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-        l1 = reverse(l1);
-        l2 = reverse(l2);
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
-        int carry = 0;
-        while (l1 != null || l2 != null || carry != 0) {
-            int sum = carry;
-            if (l1 != null) {
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            
-            ListNode node = new ListNode(sum % 10);
-            curr.next = node;
-            curr = node;
-            carry = sum / 10;
-        }return reverse(dummy.next);
-    }
-        private ListNode reverse(ListNode head) {
-        ListNode pre = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = pre;
-            pre = curr;
-            curr = next;
+    public ListNode reverse(ListNode head){
+        ListNode pre=null;
+        ListNode curr=head;
+        while(curr!=null){
+            ListNode nextnode=curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=nextnode;
         }
         return pre;
-        }
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+          l1=reverse(l1);
+          l2=reverse(l2);
+          int carry=0;
+          ListNode dummy=new ListNode(0);
+          ListNode curr=dummy;
+          while(l1!=null || l2!=null || carry!=0){
+            int sum=carry;
+ if(l1!=null){
+    sum+=l1.val;
+    l1=l1.next;
+ }
+ if(l2!=null){
+    sum+=l2.val;
+    l2=l2.next;
+ }
+ ListNode Node=new ListNode(sum%10);
+ curr.next=Node;
+ curr=Node;
+ carry=sum/10;
+
+          }
+          return reverse(dummy.next);
+    }
 }
