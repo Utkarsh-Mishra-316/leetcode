a2edd8c42c737ab563a546fa03f49d55b234bcaa
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-09-14 21:21:39 +0530

    Time: 621 ms (41.05%), Space: 0B (100%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-select product_id   from Products 
-where low_fats ='y' AND recyclable='Y' ;
+select 
+product_id
+from Products
+where low_fats='Y' AND recyclable="Y";
