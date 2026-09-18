f23533dd5968020af31900d73a8d72136a571e21
UTKARSH MISHRA <mutkarsh316@gmail.com>
2026-09-18 23:15:07 +0530

    Time: 9 ms (11.19%), Space: 172.2 MB (96.38%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,29 @@
 class Solution {
+    static int[] merge(ArrayList<Integer> l1,ArrayList<Integer> l2){
+        int [] num=new int[l1.size()+l2.size()];
+  int i=0;
+  
+  int k=0;
+   for(  i=0;i<l1.size();i++){
+   num[k]=l1.get(i);
+   k++;
+  
+   num[k]=l2.get(i);k++; 
+    }
+    return num;
+    }
     public int[] rearrangeArray(int[] nums) {
-     int ans[]=new int[nums.length];
-     int ni=1;
-     int pi=0;
-    for(int i=0;i<nums.length;i++){
-        if(nums[i]>0){
-           ans[2*pi]= nums[i];
-           pi++;
+     int [] ans=new int[nums.length];
+     ArrayList<Integer>pos=new ArrayList<>();
+     ArrayList<Integer>neg=new ArrayList<>();
+     for(int num:nums){
+        if(num>=0){
+            pos.add(num);
         }
         else{
-            ans[ni]=nums[i];
-            ni=ni+2;
+            neg.add(num);
         }
-    }  
-return ans;
+     }   
+return merge(pos,neg);
     }
 }
